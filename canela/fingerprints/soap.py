646166c89bc15b5b7404bf2c970d1070deb41701
Dylan Bayerl <16454634+bayerl@users.noreply.github.com>
2020-10-21 23:51:21 -0400

    move soap preprocessing inside click command and add filepath argument

--- canela/fingerprints/soap.py
+++ canela/fingerprints/soap.py
@@ -8,52 +8,13 @@ from canela.io.pickle import write_pickle
 from canela.fingerprints.grid import calc_grid
 
 
-path = './'
-file_cube = 'aiida-rho-ELECTRON_DENSITY-1_0.cube'
-
-print('Reading cube...')
-start = time.time()
-cube = read_cube(path + file_cube)
-elapsed = time.time() - start
-print('DONE in ', elapsed, '\n')
-
-print('Calculating grid...')
-start = time.time()
-grid = calc_grid(cube)
-elapsed = time.time() - start
-print('DONE in ', elapsed, '\n')
-print('Shape of grid:', grid.shape)
-
-struc = cube['atoms']
-rho = cube['data']
-
-print('Writing grid to disk...')
-start = time.time()
-write_pickle(grid, path + 'grid')
-elapsed = time.time() - start
-print('DONE in ', elapsed, '\n')
-
-print('Flattening rho...')
-start = time.time()
-rho = rho.flatten(order='C')  # flatten in row-major order to match grid order
-rho = rho.reshape(-1, 1)
-elapsed = time.time() - start
-print('DONE in ', elapsed, '\n')
-print('Shape of rho:', rho.shape)
-
-print('Writing rho to disk...')
-start = time.time()
-write_pickle(rho, path + 'rho')
-elapsed = time.time() - start
-print('DONE in ', elapsed, '\n')
-
-
 @click.command()
+@click.option('-filepath', default=None, help='Input cube file with data')
 @click.option('-nmax', default=2, help='Number of radial basis functions')
 @click.option('-lmax', default=2, help='Max degree of spherical harmonics')
 @click.option('-rcut', default=5.0, help='Cutoff radius in Angstrom')
 @click.option('-procs', default=1, help='Processes for parallel execution')
-def create_soap(nmax, lmax, rcut, procs):
+def create_soap(filepath, nmax, lmax, rcut, procs):
     species = ['Au']
     soap = SOAP(
         species=species,
@@ -62,6 +23,44 @@ def create_soap(nmax, lmax, rcut, procs):
         nmax=nmax,
         lmax=lmax,
     )
+    print('Reading cube...')
+    start = time.time()
+    cube = read_cube(filepath)
+    elapsed = time.time() - start
+    print('DONE in ', elapsed, '\n')
+
+    print('Calculating grid...')
+    start = time.time()
+    grid = calc_grid(cube)
+    elapsed = time.time() - start
+    print('DONE in ', elapsed, '\n')
+    print('Shape of grid:', grid.shape)
+
+    struc = cube['atoms']
+    rho = cube['data']
+
+    outpath = filepath.strip('cube')
+    
+    print('Writing grid to disk...')
+    start = time.time()
+    write_pickle(grid, outpath + 'grid')
+    elapsed = time.time() - start
+    print('DONE in ', elapsed, '\n')
+
+    print('Flattening rho...')
+    start = time.time()
+    rho = rho.flatten(order='C')  # flatten in row-major order to match grid order
+    rho = rho.reshape(-1, 1)
+    elapsed = time.time() - start
+    print('DONE in ', elapsed, '\n')
+    print('Shape of rho:', rho.shape)
+
+    print('Writing rho to disk...')
+    start = time.time()
+    write_pickle(rho, outpath + 'rho')
+    elapsed = time.time() - start
+    print('DONE in ', elapsed, '\n')
+    
     print('Calculating SOAPs on', procs, 'procs')
     print('nmax, lmax, rcut =', nmax, lmax, rcut)
     start = time.time()
@@ -72,7 +71,7 @@ def create_soap(nmax, lmax, rcut, procs):
 
     print('Writing SOAPs to disk...')
     start = time.time()
-    write_pickle(soap_struc, path
+    write_pickle(soap_struc, outpath
                  + 'soap_n_' + str(nmax)
                  + '_l_' + str(lmax)
                  + '_r_' + str(rcut)
